c091c2eb45120b985233d1fbb9a9b59ea28e555f
dawidborakiewicz <dawid.borakiewicz@student.put.poznan.pl>
2025-03-29 22:11:53 +0100

    Dawid 29.03.2025 - Lista 25 Aplikacji ich nazwy ikony i czas korzystania

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,6 +40,7 @@ android {
 }
 
 dependencies {
+    implementation("io.coil-kt:coil-compose:2.1.0")
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
--- app/src/main/java/com/example/projektinzyneiria/ScreenUsage.kt
+++ app/src/main/java/com/example/projektinzyneiria/ScreenUsage.kt
@@ -1,26 +1,47 @@
 package com.example.projektinzyneiria
 
-import androidx.activity.compose.rememberLauncherForActivityResult
-import androidx.activity.result.contract.ActivityResultContracts
-import com.example.projektinzyneiria.ui.theme.ProjektInzyneiriaTheme
-
 import android.app.AppOpsManager
 import android.app.usage.UsageEvents
 import android.app.usage.UsageStatsManager
 import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.drawable.Drawable
 import android.provider.Settings
-import androidx.compose.foundation.layout.*
-import androidx.compose.material3.*
-import androidx.compose.runtime.*
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import coil.compose.rememberAsyncImagePainter
+import com.example.projektinzyneiria.ui.theme.ProjektInzyneiriaTheme
 import kotlinx.coroutines.delay
 import java.util.concurrent.TimeUnit
+import kotlin.toString
 
 @Composable
 fun UsageScreen(modifier: Modifier = Modifier) {
@@ -49,9 +70,13 @@ fun UsageScreen(modifier: Modifier = Modifier) {
             if (hasPermission) {
                 usageData = getAppUsage(context)
             }
-            delay(TimeUnit.MINUTES.toMillis(15))
+            delay(TimeUnit.MINUTES.toMillis(5)) // Update every 5 minutes
         }
     }
+
+    val iconCache = remember { mutableMapOf<String, Drawable?>() }
+    val nameCache = remember { mutableMapOf<String, String>() }
+    val timeCache = remember { mutableMapOf<String, Long>() }
 
     Column(
         modifier = modifier.padding(16.dp),
@@ -60,7 +85,7 @@ fun UsageScreen(modifier: Modifier = Modifier) {
         if (hasPermission) {
             if (dataLoaded) {
                 Text(
-                    "App Usage (Last 24 hours)",
+                    "App Usage (Last 7 Days)",
                     style = MaterialTheme.typography.headlineSmall,
                     textAlign = TextAlign.Center
                 )
@@ -68,9 +93,36 @@ fun UsageScreen(modifier: Modifier = Modifier) {
                 if (usageData.isEmpty()) {
                     Text("No usage data found.")
                 } else {
-                    usageData.take(10).forEach { (packageName, timeSpent) ->
-                        val timeInMinutes = timeSpent / 1000 / 60
-                        Text("$packageName: $timeInMinutes minutes")
+                    LazyColumn {
+                        items(usageData.take(25)) { (packageName, timeSpent) -> // Limit to top 25 apps
+                            timeCache[packageName] = timeSpent
+                            val (appName, appIcon, appTime) = getAppInfo(context, packageName, "MainActivity", iconCache, nameCache, timeCache) // Replace "MainActivity" with the actual class name if needed
+                            val days = TimeUnit.MILLISECONDS.toDays(timeSpent)
+                            val hours = TimeUnit.MILLISECONDS.toHours(timeSpent) % 24
+                            val minutes = TimeUnit.MILLISECONDS.toMinutes(timeSpent) % 60
+                            Row(verticalAlignment = Alignment.CenterVertically) {
+                                if (appIcon != null) {
+                                    Image(
+                                        painter = rememberAsyncImagePainter(model = appIcon),
+                                        contentDescription = "$appName icon",
+                                        modifier = Modifier.size(40.dp)
+                                    )
+                                } else {
+                                    Image(
+                                        painter = painterResource(id = R.drawable.ic_launcher_foreground),
+                                        contentDescription = "Default icon",
+                                        modifier = Modifier.size(40.dp)
+                                    )
+                                }
+                                Spacer(modifier = Modifier.width(8.dp))
+                                Text(buildString {
+                                    append("$appName: ")
+                                    if (days > 0) append("$days dni ")
+                                    if (days > 0 || hours > 0) append("$hours godzin ")
+                                    append("$minutes minut")
+                                })
+                            }
+                        }
                     }
                 }
             } else {
@@ -137,7 +189,7 @@ fun getAppUsage(context: Context): List<Pair<String, Long>> {
     val usageStatsManager =
         context.getSystemService(Context.USAGE_STATS_SERVICE) as UsageStatsManager
     val endTime = System.currentTimeMillis()
-    val startTime = endTime - TimeUnit.DAYS.toMillis(1)
+    val startTime = endTime - TimeUnit.DAYS.toMillis(7) // Last 7 days
 
     val usageEvents = usageStatsManager.queryEvents(startTime, endTime)
     val resumeTimes = mutableMapOf<String, Long>()
@@ -164,8 +216,48 @@ fun getAppUsage(context: Context): List<Pair<String, Long>> {
     }
 
     return usageDurations.entries
-        .sortedByDescending { it.value }
+        .sortedByDescending { it.value } // Sort by usage duration
         .map { it.key to it.value }
+
+
+}
+
+
+fun getAppInfo(
+    context: Context,
+    packageName: String,
+    className: String,
+    iconCache: MutableMap<String, Drawable?>,
+    nameCache: MutableMap<String, String>,
+    timeCache: MutableMap<String, Long>
+): Triple<String, Drawable?, Long?> {
+    val packageManager = context.packageManager
+    return try {
+        val appInfo = packageManager.getApplicationInfo(packageName, 0)
+        val appName = nameCache.getOrPut(packageName) { packageManager.getApplicationLabel(appInfo).toString() }
+        val appIcon = iconCache.getOrPut(packageName) { getIcon(context, packageName, className) }
+        val appTime = timeCache[packageName]
+        Triple(appName, appIcon, appTime)
+    } catch (e: PackageManager.NameNotFoundException) {
+        Triple(packageName, null, null)
+    }
+}
+
+fun getIcon(context: Context, packageName: String, className: String): Drawable? {
+    var drawable: Drawable? = null
+    try {
+        val intent = Intent(Intent.ACTION_MAIN)
+        intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+        intent.setClassName(packageName, className)
+        drawable = context.packageManager.getActivityIcon(intent)
+    } catch (e: Exception) {
+        try {
+            drawable = context.packageManager.getApplicationIcon(packageName)
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+    }
+    return drawable
 }
 
 @Preview(showBackground = true)
